1af0bb2b1263be152d80da8a66ce1ed71e756771
ArthurTab <arthursalomon4@gmail.com>
2025-06-20 11:57:26 +0200

    limit forcebrute

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -36,6 +36,13 @@ class AuthController extends Controller
             exit;
         }
 
+        if (isset($_SESSION['blocked_until']) && time() < $_SESSION['blocked_until']) {
+            $seconds = $_SESSION['blocked_until'] - time();
+            set_flash('error', "Trop de tentatives. Réessayez dans $seconds seconde(s).");
+            $this->render('auth/login', ['error' => $error]);
+            exit;
+        }
+
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $username = $_POST['username'] ?? '';
             $password = $_POST['password'] ?? '';
@@ -49,12 +56,19 @@ class AuthController extends Controller
             if ($result) {
                 $_SESSION['user'] = $result;
                 $_SESSION['user_role'] = $result['role'];
-//                setcookie('user_role', $result['role'], time() + 3600, '/');
                 set_flash('success', 'Connexion réussie !');
                 header(REDIRECT_HEADER . base_url());
                 exit;
             } else {
                 set_flash('error', 'Identifiants invalides');
+                $_SESSION['attempts'] = ($_SESSION['attempts'] ?? 0) + 1;
+                if ($_SESSION['attempts'] >= 5) {
+                    $_SESSION['blocked_until'] = time() + 60; // 60 seconds to test
+                    $_SESSION['attempts'] = 0;
+                    set_flash('error', 'Trop de tentatives. Réessayez dans 1 minute.');
+                } else {
+                    sleep(2);
+                }
             }
         }
 
